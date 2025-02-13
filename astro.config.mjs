b8481cbeb802af33c57fabfa8fb9748b25c1c41a
schmay33 <nschmdt@gmail.com>
2025-02-13 13:55:30 +0800

    move styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/wayanbaliguide/', // Set this to your repository name
+  site: 'https://schmay33.github.io',
+  base: '/wayanbaliguide',
 });
